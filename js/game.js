875b94d2c81d7bfe781290e9c9047ae8fddb1def
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-13 22:48:39 +0000

    added js

---
+++ js/game.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
