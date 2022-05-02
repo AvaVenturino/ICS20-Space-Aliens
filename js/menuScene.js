37c36f9a7376d562b22a8b4f21f207edc61a33b2
AvaVenturino <AvaVenturino@users.noreply.github.com>
2022-05-02 21:38:14 +0000

    Prettified Code!

--- js/menuScene.js
+++ js/menuScene.js
@@ -36,8 +36,8 @@ class MenuScene extends Phaser.Scene {
    */
   preload() {
     console.log("Menu Scene")
-    this.load.image('menuSceneBackground', 'assets/aliens_screen_image2.jpg')
-    this.load.image('startButton', 'assets/start.png')
+    this.load.image("menuSceneBackground", "assets/aliens_screen_image2.jpg")
+    this.load.image("startButton", "assets/start.png")
   }
 
   /**
@@ -46,13 +46,15 @@ class MenuScene extends Phaser.Scene {
    * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground').setScale(2.75)
+    this.menuSceneBackgroundImage = this.add
+      .sprite(0, 0, "menuSceneBackground")
+      .setScale(2.75)
     this.menuSceneBackgroundImage.x = 1920 / 2
     this.menuSceneBackgroundImage.y = 1080 / 2
 
-    this.startButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton")
     this.startButton.setInteractive({ userHandCursor: true })
-    this.startButton.on('pointerdown', () => this.clickButton())
+    this.startButton.on("pointerdown", () => this.clickButton())
     //pass
   }
 
@@ -62,13 +64,12 @@ class MenuScene extends Phaser.Scene {
    * @param {number} time - the current time.
    * @param {number} delta - The delta time in ms since the last frame.
    */
-  update(time, delta) {
+  update(time, delta) {}
+
+  clickButton() {
+    this.scene.start("gameScene")
   }
-    
-  clickButton () {
-      this.scene.start('gameScene')
-    }
-    // pass
-  }
+  // pass
+}
 
 export default MenuScene
