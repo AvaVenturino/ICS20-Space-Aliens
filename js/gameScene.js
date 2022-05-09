36c7e96ccf1fec0c3e01a795315b4dd2ee3cef39
AvaVenturino <AvaVenturino@users.noreply.github.com>
2022-05-09 16:49:46 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,45 +10,43 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-
   constructor() {
-    super({ key: 'gameScene' })
+    super({ key: "gameScene" })
 
     this.background = null
     this.ship = null
   }
 
   init(data) {
-    this.cameras.main.setBackgroundColor('#ffffff')
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
 
   preload() {
-    console.log('Game Scene')
+    console.log("Game Scene")
     //images
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
   }
 
   create(data) {
-    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0,0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
 
-
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
   }
 
   update(time, delta) {
     // called 60 times a second, hopefully!
 
-    const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keyLeftObj = this.input.keyboard.addKey("LEFT")
+    const keyRightObj = this.input.keyboard.addKey("RIGHT")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
       if (this.ship.x < 0) {
         this.ship.x = 0
       }
-    }  
+    }
 
     if (keyRightObj.isDown === true) {
       this.ship.x += 15
