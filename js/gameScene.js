44344a2e29c69b5e91f4ca72ba252cbe4683cf19
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-26 17:09:30 +0000

    changed gamescene

--- js/gameScene.js
+++ js/gameScene.js
@@ -39,6 +39,7 @@ class GameScene extends Phaser.Scene {
       fill: "#ffffff",
       align: "center",
     }
+    this.gameOverTextStyle = { font: "65px Ariel", fill: "#ff0000", align: "center", }
   }
 
   /**
@@ -65,6 +66,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio("laser", "assets/laser1.wav")
     this.load.audio("explosion", "assets/barrelExploding.wav")
+    this.load.audio("bomb", "assets/bomb.wav")
   }
 
   /**
@@ -106,6 +108,17 @@ class GameScene extends Phaser.Scene {
         this.createAlien()
       }.bind(this)
     )
+
+    // Collisions between ship and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over:\nClick to play again', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ userHandCurser: true })
+      this.gameOverText.on('pointerdown', () => this.scenestart('gameScene'))
+    }.bind(this))
   }
 
   /**
