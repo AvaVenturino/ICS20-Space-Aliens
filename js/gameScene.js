e7394fa457139f89d083f9bf61037cb924e3dafb
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-26 16:23:12 +0000

    changed comment

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,7 +10,9 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-  // create an alien
+/**
+ * create an alien
+ */
   createAlien() {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
     let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50
